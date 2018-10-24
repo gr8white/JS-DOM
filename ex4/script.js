5beeb521c5cdb30de3d63e143872ffee7b322f36
Derrick White <gr8white@uga.edu>
2018-10-23 20:27:27 -0500

    Finished exercises 4-7

--- ex4/script.js
+++ ex4/script.js
@@ -1,2 +1,14 @@
 // Make all of the boxes blue when you click the button
 // Notice that all the boxes have "class" instead of "id"
+
+var button = document.getElementById('myButton')
+var element1 = document.getElementsByClassName('box')
+
+button.addEventListener('click', function () {
+  for (let i = 0; i < element1.length; i++) {
+    element1[i].style.backgroundColor = 'blue'
+  }
+  // element1[0].style.backgroundColor = 'blue'
+  // element1[1].style.backgroundColor = 'blue'
+  // element1[2].style.backgroundColor = 'blue'
+})
--- ex5/script.js
+++ ex5/script.js
@@ -1,3 +1,13 @@
 // Add a box to "boxContainer" everytime you click the button
 // HINT: Look up createElement(), appendChild()
 // HINT HINT: You can add the "box" class to elements like so: https://www.w3schools.com/howto/howto_js_add_class.asp
+
+var button = document.getElementById('myButton')
+var boxContainer = document.getElementById('boxContainer')
+// var element1 = document.getElementsByClassName('box');
+
+button.addEventListener('click', function () {
+  var newDiv = document.createElement('div')
+  newDiv.classList.add('box')
+  boxContainer.appendChild(newDiv)
+})
